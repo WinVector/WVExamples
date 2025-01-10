f5ebea81e4710103b8133122f153c6888d4763dd
Nina Zumel <nzumel@win-vector.com>
2025-01-09 16:01:04 -0800

    basic framework up, with test articles and projects

---
+++ website/_data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Win Vector Examples",
+	url: "https://winvector.github.io/WVExamples",
+	language: "en",
+	description: "Win Vector technical articles and example code",
+	author: {
+		name: "Nina Zumel and John Mount",
+		email: "contact@win-vector.com",
+		url: "https://win-vector.com"
+	}
+}
---
+++ website/content/about.md
@@ -0,0 +1,9 @@
+---js
+const eleventyNavigation = {
+	key: "About",
+	order: 3
+};
+---
+# About
+
+This is a site for articles and supporting source code to support the [Win Vector blog](https://win-vector.com/).
